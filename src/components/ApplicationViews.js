cb03b4b764990fd9cf202b00376a486031407845
ramirors1 <ramirezr@protonmail.com>
2021-07-08 14:14:10 -0500

    finished chap6

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
-import { AnimalCard } from "./animal/AnimalCard"
+// import { AnimalCard } from "./animal/AnimalCard"
 import { LocationCard } from "./Locations"
 import { CustomerCard } from "./Customers"
 import { EmployeeCard } from "./Employees"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
 
 export const ApplicationViews = () => {
     return (
@@ -14,10 +16,16 @@ export const ApplicationViews = () => {
                 <Home />
             </Route>
 
-            {/* Render the animal list when http://localhost:3000/animals */}
+            <AnimalProvider>
+            <Route exact path="/animals">
+            <AnimalList />
+            </Route>
+            </AnimalProvider>
+
+            {/* Render the animal list when http://localhost:3000/animals
             <Route path="/animals">
                 <AnimalCard />
-            </Route>
+            </Route> */}
 
             {/* Render the location list when http://localhost:3000/animals */}
             <Route path="/locations">
--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,62 +1,58 @@
 import React from "react"
-import { AnimalCard } from "./animal/AnimalCard"
-import "./animal/Animal.css"
-import "./Kennel.css"
-import { CustomerCard } from "./Customers"
-import { EmployeeCard } from "./Employees"
-import { LocationCard } from "./Locations"
-import "./Customers.css"
-import "./Employees.css"
-import "./Locations.css"
-import { PropsAndState } from "./PropsAndState"
 import { NavBar } from "./nav/NavBar"
 import { ApplicationViews } from "./ApplicationViews"
+import "./Kennel.css"
 
-
-//Look carefully at the <article> tag. In React, we add classes to a component with `className` instead of `class`.
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <PropsAndState yourName="Ramiro" />
-        
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
-
-        <h2>Customers</h2>
-        <article className="customers">
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-            
-        </article>
-
-        <h2>Employees</h2>
-        <article className="employees">
-            <EmployeeCard />
-            <EmployeeCard />
-            <EmployeeCard />
-        </article>
-
-        <h2>Locations</h2>
-        <article className="locations">
-            <LocationCard />
-            <LocationCard />
-            <LocationCard />
-        </article>
-
         <NavBar />
         <ApplicationViews />
-  
     </>
-)
+)//Look carefully at the <article> tag. In React, we add classes to a component with `className` instead of `class`.
+// export const Kennel = () => (
+//     <>
+//      <NavBar />
+//         <ApplicationViews />
+  
+//         <h2>Nashville Kennels</h2>
+//         <small>Loving care when you're not there.</small>
+
+//         <address>
+//             <div>Visit Us at the Nashville North Location</div>
+//             <div>500 Puppy Way</div>
+//         </address>
+
+//         <PropsAndState yourName="Ramiro" />
+        
+//         <h2>Animals</h2>
+//         <article className="animals">
+//             <AnimalCard />
+//             <AnimalCard />
+//             <AnimalCard />
+//         </article>
+
+//         <h2>Customers</h2>
+//         <article className="customers">
+//             <CustomerCard />
+//             <CustomerCard />
+//             <CustomerCard />
+            
+//         </article>
+
+//         <h2>Employees</h2>
+//         <article className="employees">
+//             <EmployeeCard />
+//             <EmployeeCard />
+//             <EmployeeCard />
+//         </article>
+
+//         <h2>Locations</h2>
+//         <article className="locations">
+//             <LocationCard />
+//             <LocationCard />
+//             <LocationCard />
+//         </article>
+
+       
+//     </>
+// )
